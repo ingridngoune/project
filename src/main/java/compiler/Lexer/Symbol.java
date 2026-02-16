e993ec83019be3518999176f94442470e12291c1
ingrid <ingrid.ngounekenfack@condorce.be>
2026-02-16 15:44:44 +0100

    add of symbol type and attribute for symbol class

--- src/main/java/compiler/Lexer/Symbol.java
+++ src/main/java/compiler/Lexer/Symbol.java
@@ -1,5 +1,29 @@
 package compiler.Lexer;
 
 public class Symbol {
+    private final SymbolType type;
+    private final String value;
 
+    public Symbol(SymbolType type,String value){
+        this.type=type;
+        this.value=value;
+    }
+
+    public SymbolType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        if(value==null) {
+            return "<" + type + ">";
+        }
+        return "<" + type +
+                "," + value +
+                ">";
+    }
 }
--- src/main/java/compiler/Lexer/SymbolType.java
+++ src/main/java/compiler/Lexer/SymbolType.java
@@ -1,2 +1,67 @@
-package compiler.Lexer;public enum SymbolType {
+package compiler.Lexer;
+
+public enum SymbolType {
+
+    //Webographie
+    //https://medium.com/@enzojade62/step-by-step-building-a-lexer-in-java-for-tokenizing-source-code-ac4f1d91326f
+    //https://docs.oracle.com/javase/specs/jls/se7/html/jls-3.html
+
+    //list of keywords
+    FINAL,
+    COLL,
+    FOR,
+    WHILE,
+    IF,
+    ELSE,
+    RETURN,
+    NOT,
+    ARRAY,
+
+    //list of types
+    INT_TYPE,
+    FLOAT_TYPE,
+    BOOL_TYPE,
+    STRING_TYPE,
+
+    //list of values
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    BOOL_LITERAL,
+
+    IDENTIFIER,
+    COLLECTION,
+
+    //list of logical Opererators
+    ASSIGN,
+    EQUAL,
+    NOT_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+
+
+    //lis of boolean Operator
+    AND,
+    OR,
+
+    //list of artihmetical operators
+
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULO,
+
+    ARROW,
+
+    //list of special caracter
+
+    LEFT_PAREN,RIGHT_PAREN,
+    LEFT_BRACKET,RIGHT_BRACKET,
+    LEFT_BRACE,RIGHT_BRACE,
+    DOT,SEMILCOLON,COMMA,
+
+    EOF
 }
